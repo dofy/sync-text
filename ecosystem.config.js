5c544ad412ee3b35d4568fb0abb83a02dec2aae5
Seven Yu <dofyyu@gmail.com>
2023-04-16 22:27:01 +0800

    add ecosystem.config.js file

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "sync-text",
+      script: "yarn start",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "1G",
+      ignore_watch: ["public"],
+      env: {
+        PORT: 9000,
+        watch: true,
+        NODE_ENV: "development",
+      },
+      env_production: {
+        PORT: 9100,
+        watch: false,
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
